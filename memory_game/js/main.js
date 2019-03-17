4c53ac4266d326ffe012b604dc4024337cc393b7
Ritika Goel <ritika2654@gmail.com>
2019-03-17 15:06:15 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,43 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1])
 	{
-		console.log("You found a match!");
+		alert("You found a match!");
 	}
 	else
 	{
-		console.log("Sorry, try again");
+		alert("Sorry, try again");
 	}
 }
 
 var flipcard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped: " + cards[cardId].rank);
+	console.log("Of the suit: " + cards[cardId].suit);
+	console.log("Card Image: " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length === 2)
 	{
 		checkForMatch();
