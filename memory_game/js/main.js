4ee45cd86af66210a75b8d89b78e454fecff260d
Ritika Goel <ritika2654@gmail.com>
2019-03-17 12:55:41 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,25 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped: " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped: " + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2)
-{
+var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1])
 	{
-		alert("You found a match!");
+		console.log("You found a match!");
 	}
 	else
 	{
-		alert("Sorry, try again");
+		console.log("Sorry, try again");
 	}
 }
+
+var flipcard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2)
+	{
+		checkForMatch();
+	}
+}
+
+flipcard(0);
+flipcard(2);
