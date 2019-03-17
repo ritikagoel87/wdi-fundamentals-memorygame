06bb4a52c5368bb992214e29bb4fe7dce6ee5a85
Ritika Goel <ritika2654@gmail.com>
2019-03-17 21:14:15 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,27 +22,55 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipcard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+var resetCards = function() {
+	cardsInPlay = [];
+	for(var j = 0; j < cards.length; j++)
+	{
+		document.getElementsByTagName('img')[j].setAttribute('src', 'images/back.png');
+	}
+}
+
 var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1])
 	{
 		alert("You found a match!");
+		cardsInPlay = [];
 	}
 	else
 	{
 		alert("Sorry, try again");
+		resetCards();
 	}
 }
 
-var flipcard = function(cardId) {
-	console.log("User flipped: " + cards[cardId].rank);
-	console.log("Of the suit: " + cards[cardId].suit);
-	console.log("Card Image: " + cards[cardId].cardImage);
+var flipcard = function() {
+	var cardId = this.getAttribute('data-id');
+	//console.log("User flipped: " + cards[cardId].rank);
+	//console.log("Of the suit: " + cards[cardId].suit);
+	//console.log("Card Image: " + cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+
+
+
 	if(cardsInPlay.length === 2)
 	{
-		checkForMatch();
+		setTimeout(function(){ checkForMatch(); }, 200);
+		
 	}
 }
 
-flipcard(0);
-flipcard(2);
+document.getElementById('reset').addEventListener('click', resetCards);
+
+createBoard();
